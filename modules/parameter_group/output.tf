d5253d2e0e8e4e4f7e57a151492fbe95b20e5553
Rajesh Jonnalagadda <rajesh.jonnalagadda@thoughtworks.com>
2025-08-11 12:49:56 +0530

    Fixing Terraform validations

--- modules/parameter_group/output.tf
+++ modules/parameter_group/output.tf
@@ -1,4 +1,4 @@
 output id {
   description = "This is the ID of the RDS Parameter Group"
-  value =try(aws_db_parameter_group.this[0].id,null)
+  value =try(aws_db_parameter_group.db_parameter_group[0].id,null)
 }
